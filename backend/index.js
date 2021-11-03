e1697592227413ebbd14dac898cab76af51053f0
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-03 20:29:46 +0200

    Connect entry point to DB

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv  from "dotenv";
+import connectDB from "./config/db.js";
 
+// initiate dotenv
+dotenv.config();
 
-const app     = express();
+// connect to MongoDB
+connectDB();
 
+// initiate express
+const app = express();
+
+// root
 app.get("/", (req, res) => {
 
     res.send("API running");
 });
 
+// port to default to 5000 if there is no PORT in process environment
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
