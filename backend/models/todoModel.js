1a2382bddd602b5c21b512f36a04738c7cfc9dee
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-03 20:13:27 +0200

    Model for todo items added. Model is for MongoDB. It uses mongoose

---
+++ backend/models/todoModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// Simple MongoDB model for todo items
+const todoSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+},
+{
+        timestamps: true
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+export default Todo;
