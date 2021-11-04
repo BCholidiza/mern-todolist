718c88f2e6202fcccfcf21a70b253a524f2a6454
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-04 20:12:44 +0200

    Add route to get item by id

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -12,3 +12,26 @@ router.get("/", asyncHandler( async (req, res) => {
     const todo = await Todo.find({});
     res.json(todo);
 }));
+
+
+// @description     Fetch a single todo
+// @route           GET /api/todos/:id
+// @access          public
+router.get("/:id", asyncHandler( async (req, res) => {
+
+    // Runs through array and tries to find item in array as per req.params.id (url parameters)
+    
+    // this finds id in mongo using the todo model we created
+    const todo = await Todo.findById(req.params.id);
+
+    if (todo){
+        res.json(todo);
+    }
+    else {
+        res.status(404);
+        throw new Error("To not found");
+    }
+}));
+
+
+export default router;
