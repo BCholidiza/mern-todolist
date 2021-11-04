7356ad7b65c725ac94b361c0dcb6aadc9c08d587
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-04 19:26:58 +0200

    User error handlers and routing to get items

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv  from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware";
 
 // initiate dotenv
 dotenv.config();
@@ -16,6 +17,11 @@ app.get("/", (req, res) => {
 
     res.send("API running");
 });
+
+app.use("/api/todo", todoRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 // port to default to 5000 if there is no PORT in process environment
 const PORT = process.env.PORT || 5000
