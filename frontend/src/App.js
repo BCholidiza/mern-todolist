cd7d1c19ff68cc5b8be9746976f80247c23b4ef3
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-06 18:38:38 +0200

    Connected frontend to backend using axios. created temporary state to store data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,27 @@ import './App.css';
 import AddToDo from './components/AddTodo';
 import DisplayTodo from './components/DisplayTodo';
 import FilterTodo from './components/FilterTodo';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 
 function App() {
 
+    const [todos, setTodos] = useState([]);
+    useEffect( () => {
+
+        const fetchTodos = async () => {
+
+            const { data } = await axios.get("/api/todos");
+        }
+        axios.get("api/todos");
+
+    }, []);
+
     return (
         <div className="App">
+{/*             <nav className="navigation-todo">
+                 
+            </nav> */}
             <AddToDo />
             <DisplayTodo />
             <FilterTodo />
