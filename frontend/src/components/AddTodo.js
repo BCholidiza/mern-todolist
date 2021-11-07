f2f0ef55d2d7344a27ee8b1292998b43a85d3f11
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-07 20:54:19 +0200

    Add logic to add todo item when user types and presses enter

--- frontend/src/components/AddTodo.js
+++ frontend/src/components/AddTodo.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const AddToDo = () => {
 
+    const [newTodo, setNewTodo] = useState({});
+
+    // function handles addition of a new todo item
+    const handleAdd = (event) => {
+    
+        // if this is an enter button, then add to state
+        if (event.keyCode === 13){
+
+            const addTodo = { title: event.target.value, isCompleted: false, isDeleted: false };
+            setNewTodo(addTodo);
+            event.target.value = "";
+            
+            // post this to backend
+            const response = async () => await axios.post("api/todos/add", newTodo);
+        }
+    }
+
+    useEffect( () => {
+                
+        //const response = await axios.post("api/todos/add", newTodo);
+
+    }, [newTodo])
 
     return (  
         <div className="add-todo">
-            <input className="new-todo" type='text'/>
+            <input className="new-todo" type='text' onKeyUp={ (e)=>handleAdd(e) } />
         </div>
     );
 }
