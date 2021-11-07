841a84dd51b73176f0d4c219c6cecc9f94de51ad
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-07 19:45:52 +0200

    dispatch listtodos action. refresh on pathname which will be updated based on route selected by user

--- frontend/src/components/DisplayTodo.js
+++ frontend/src/components/DisplayTodo.js
@@ -1,8 +1,28 @@
-const DisplayTodo = () => {
-    return (  
+import { useDispatch, useSelector } from "react-redux";
+import { listTodos, activeTodos, completedTodos } from "../actions/todoActions"
+import React, { useEffect } from 'react';
+import { useLocation } from "react-router-dom";
 
+const DisplayTodo = () => {
+    
+    const pathname = useLocation().pathname;
+
+    const dispatch = useDispatch();
+
+    const todoList = useSelector(state => state.todoList);
+    const { loading, error, todos } = todoList;
+
+    useEffect( () => {
+
+        dispatch(listTodos(pathname));
+    }, [dispatch, pathname]);
+
+    return (
         <div className="display-todo">
-            Display To Do
+            
+            { loading ? <div>Loading...</div> : 
+               error ? <div>{ error }</div> : todos.map( todo => <div className="list-todo" key={ todo._id } >{ todo.title }</div>)
+            }
         </div>
     );
 }
