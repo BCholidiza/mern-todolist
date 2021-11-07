fd2a941a63566b6df07e50ce1b51d42160f8888b
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-07 19:44:22 +0200

    Remove unused constants

--- frontend/src/reducers/todoReducers.js
+++ frontend/src/reducers/todoReducers.js
@@ -1,17 +1,16 @@
-import { TODO_LIST_REQUEST, TODO_LIST_SUCCESS, TODO_LIST_FAIL } from "../constants/todoConstants";
+import { TODO_LIST_REQUEST, TODO_LIST_SUCCESS, TODO_LIST_FAIL,
+         TODO_ACTIVE_REQUEST, TODO_ACTIVE_SUCCESS, TODO_ACTIVE_FAIL,
+         TODO_COMPLETED_REQUEST, TODO_COMPLETED_SUCCESS, TODO_COMPLETED_FAIL } from "../constants/todoConstants";
 
 export const todoListReducer = (state = { todos: []}, action) => {
 
     switch (action.type) {
         case TODO_LIST_REQUEST:
             return { loading: true, todos: [] };
-            break;
         case TODO_LIST_SUCCESS:
             return { loading: false, todos: action.payload };
-            break
         case TODO_LIST_FAIL:
             return { loading: false, error: action.payload };
-            break;
         default:
             return state;
     }
