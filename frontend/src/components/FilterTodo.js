f12450e305560783627a639a563af52a83501d8a
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-10 15:50:16 +0200

    Add function to call remove route when "Clear completed" is clicked

--- frontend/src/components/FilterTodo.js
+++ frontend/src/components/FilterTodo.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom"
+import axios from "axios";
 
 const FilterTodo = () => {
+    
+    const removeRequest = async () => await axios.post(`api/todos/remove`);
+    
     return (  
         <nav className="filter-todo">
             <span><Link to="/">All</Link></span>
             <span><Link to="/active">Active</Link></span>
             <span><Link to="/completed">Completed</Link></span>
-            <span><Link to="#">Clear Completed</Link></span>
+            <span><Link onClick={ removeRequest } to="">Clear Completed</Link></span>
         </nav>
     );
 }
