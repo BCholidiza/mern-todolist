51d0c65c7519c5dfc34cdb5e439784e37ab5561e
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-11 08:55:14 +0200

    Add count for active todo items

--- frontend/src/components/FilterTodo.js
+++ frontend/src/components/FilterTodo.js
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom"
 import axios from "axios";
 
-const FilterTodo = () => {
+const FilterTodo = (props) => {
     
-    const removeRequest = async () => await axios.post(`api/todos/remove`);
+    const num = props.noTodoLeft.length;
+    
+    const removeRequest = async () => {
+        
+        await axios.post(`api/todos/remove`);
+    }
     
     return (  
-        <nav className="filter-todo">
-            <span><Link to="/">All</Link></span>
-            <span><Link to="/active">Active</Link></span>
-            <span><Link to="/completed">Completed</Link></span>
-            <span><Link onClick={ removeRequest } to="">Clear Completed</Link></span>
-        </nav>
+        <li className="filter-todo">
+            { num ? <span>{ num } items left</span> : <span></span> }
+            <Link to="/">All</Link>
+            <Link to="/active">Active</Link>
+            <Link to="/completed">Completed</Link>
+            <Link onClick={ removeRequest } to="">Clear Completed</Link>
+        </li>
     );
 }
  
