0547fefd515433b5650b5030ac6af485a809d637
BCholidiza <cholidiza.bomi@gmail.com>
2021-11-11 08:54:38 +0200

    Moved Filter component to Display component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
-import AddToDo from './components/AddTodo';
 import DisplayTodo from './components/DisplayTodo';
-import FilterTodo from './components/FilterTodo';
+import AddToDo from './components/AddTodo';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
     return (
         <Router>
+            
             <div className="App">
+                <h2>TO DO</h2>
+                <AddToDo />
                 <div className="content">
-                    <AddToDo />
                     <Routes>
                         <Route exact path="/" element={ <DisplayTodo /> } />
                         <Route exact path="/active" element={ <DisplayTodo /> } />
                         <Route exact path="/completed" element={ <DisplayTodo /> } />
                         <Route exact path="/remove"  element={ <DisplayTodo /> } />
                     </Routes>
-                    <FilterTodo />
                 </div>
             </div>
         </Router>
